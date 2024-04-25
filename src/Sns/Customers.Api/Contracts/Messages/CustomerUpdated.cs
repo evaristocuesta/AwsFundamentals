24943d8c79e63767dfa7a52de205473c1e93732f
evaristocuesta <evaristocg@gmail.com>
2024-04-25 21:28:04 +0200

    Add consumers api for sns

---
+++ src/Sns/Customers.Api/Contracts/Messages/CustomerUpdated.cs
@@ -0,0 +1,10 @@
+﻿namespace Customers.Api.Contracts.Messages;
+
+public class CustomerUpdated
+{
+    public required Guid Id { get; init; }
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required string GitHubUserName { get; init; }
+    public required DateTime DateOfBirh { get; init; }
+}
