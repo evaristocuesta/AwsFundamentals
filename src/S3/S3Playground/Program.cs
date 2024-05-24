5f6692db54aa9f9b063e4df280f4ebfec91e525d
evaristocuesta <evaristocg@gmail.com>
2024-05-24 19:50:51 +0200

    Upload files to S3

--- src/S3/S3Playground/Program.cs
+++ src/S3/S3Playground/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine();
+﻿
+using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+await using var inputImageStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+
+var putImageRequest = new PutObjectRequest
+{
+    BucketName = "ecgs3bucket", 
+    Key = "images/face.jpg", 
+    ContentType = "image/jpeg",
+    InputStream = inputImageStream
+};
+
+await s3Client.PutObjectAsync(putImageRequest);
+
+await using var inputCsvStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putCsvRequest = new PutObjectRequest
+{
+    BucketName = "ecgs3bucket",
+    Key = "files/movies.csv",
+    ContentType = "text/csv",
+    InputStream = inputCsvStream
+};
+
+await s3Client.PutObjectAsync(putCsvRequest);
