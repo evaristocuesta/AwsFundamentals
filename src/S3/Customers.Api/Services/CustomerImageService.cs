e886e1ccf414b026801ec746cab128ea0296cfb1
evaristocuesta <evaristocg@gmail.com>
2024-05-25 11:35:18 +0200

    Add S3 and secrets manager examples

---
+++ src/S3/Customers.Api/Services/CustomerImageService.cs
@@ -0,0 +1,57 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Customers.Api.Services;
+
+public class CustomerImageService : ICustomerImageService
+{
+    private readonly IAmazonS3 _amazonS3;
+
+    public CustomerImageService(IAmazonS3 amazonS3)
+    {
+        _amazonS3 = amazonS3;
+    }
+
+    private readonly string BUCKET_NAME = "ecgs3bucket";
+
+
+    public async Task<DeleteObjectResponse> DeleteImageAsync(Guid id)
+    {
+        var deleteObjectRequest = new DeleteObjectRequest
+        {
+            BucketName = BUCKET_NAME,
+            Key = $"images/{id}"
+        };
+
+        return await _amazonS3.DeleteObjectAsync(deleteObjectRequest);
+    }
+
+    public async Task<GetObjectResponse> GetImageAsync(Guid id)
+    {
+        var getObjectRequest = new GetObjectRequest
+        {
+            BucketName = BUCKET_NAME,
+            Key = $"images/{id}"
+        };
+
+        return await _amazonS3.GetObjectAsync(getObjectRequest);
+    }
+
+    public async Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile file)
+    {
+        var putObjectRequest = new PutObjectRequest
+        {
+            BucketName = BUCKET_NAME,
+            Key = $"images/{id}",
+            ContentType = file.ContentType,
+            InputStream = file.OpenReadStream(),
+            Metadata =
+            {
+                ["x-amz-meta-originalname"] = file.FileName,
+                ["x-amz-meta-extension"] = Path.GetExtension(file.FileName)
+            }
+        };
+
+        return await _amazonS3.PutObjectAsync(putObjectRequest);
+    }
+}
---
+++ src/SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,59 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+try
+{
+    var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionRequest);
+
+    foreach (var version in versionResponse.Versions)
+    {
+        Console.WriteLine($"{version.VersionId} - {version.CreatedDate}");
+    }
+}
+catch (ResourceNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey", 
+    VersionStage = "AWSCURRENT"
+};
+
+try
+{
+    var response = await secretsManagerClient.GetSecretValueAsync(request);
+    Console.WriteLine(response.SecretString);
+}
+catch (ResourceNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+try
+{
+    var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+    Console.WriteLine(describeResponse.CreatedDate);
+}
+catch (ResourceNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
